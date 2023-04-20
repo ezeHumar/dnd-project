355f2c05169ccf729b0ad3cb7d5e9b079f87e73d
ezeHumar <humarezequiel@gmail.com>
2023-04-20 15:14:29 -0300

    formatting

--- src/main/java/com/example/dndprojectspring/exception/ForbiddenException.java
+++ src/main/java/com/example/dndprojectspring/exception/ForbiddenException.java
@@ -3,6 +3,7 @@ package com.example.dndprojectspring.exception;
 import org.springframework.http.HttpStatus;
 
 public class ForbiddenException extends HttpErrorException{
+
     public ForbiddenException(String errorMessage, RuntimeException e){
         super(errorMessage, e, HttpStatus.FORBIDDEN);
     }
